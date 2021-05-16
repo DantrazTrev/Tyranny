9a701fd6c3d6e280637cc776a0ae3d20ac34d483
Ayush Pratap Singh <>
2021-05-16 21:18:22 +0000

    Game functions model

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -5,11 +5,7 @@ export default class Game extends React.Component {
 
   constructor(props) {
   super(props);
-  this.state={
-    name:"",
-    start:"",
-    
-  }
+
   this.tethy=this.tethy.bind(this);
 
   this.go=this.go.bind(this);
@@ -27,7 +23,150 @@ go(e){
  
 
 }
+upday(e){
+  e.preventDefault();
+  console.log("tmyk")
 
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/upday/')
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+fight(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/fight/')
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+day(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/day/')
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+me(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/me/')
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+kar(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/kar/'+e.target.value)
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+undi(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/undi/'+e.target.value)
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+undi(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/undi/'+e.target.value)
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+di(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/di/'+e.target.value)
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+trusty(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.post(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/trusty/'+e.target.value,{trust:ver})
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+rewind(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.post(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.name+'/rewind/',{day:e.target.value})
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+disc(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+   axios.post(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.anme+'/disc/',{name:e.target.value})
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
+inf(e){
+  e.preventDefault();
+  console.log("tmyk")
+
+  axios.post(`https://the-butterfly.dantraztrev.repl.co/api/game/`+this.props.anme+'/inf/',{name:e.target.value})
+      .then(res => {
+               console.log(res);
+              this.setState({start:<div class="start"><div class="start-con" onClick={this.props.chars}>{res.data.status} at the alpha server</div></div>})
+                     })
+ 
+
+}
 tethy(e){
 this.setState({name: e.target.value});
 
