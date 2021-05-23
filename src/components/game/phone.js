3c449b996e4a25f48ac485536e9c86b1dc0feec0
Ayush PS <ayushpratap16@gmail.com>
2021-05-23 18:28:05 +0530

    Phone model base

---
+++ src/components/game/phone.js
@@ -0,0 +1,35 @@
+
+
+
+return (
+    <div class="lgcell">
+  <div class="cellbg">
+    <div class="celltop">
+        <div style={{float:"left"}}><i class="fas fa-circle"></i><i class="fas fa-circle"></i><i class="fas fa-circle"></i><i class="fas fa-circle"></i><i class="fas fa-circle"></i><i class="fas fa-wifi"></i></div>17:09<div style={{float:"right"}}><i class="fas fa-battery-three-quarters"></i></div>
+    </div>
+    <div class="cellheader">
+      <div style={{float:"left"}}> Edit</div>Contacts <div style={{float:"right"}}><i class="fas fa-plus"></i></div>
+    </div>
+    <div class="cellcont">
+      <div class="celf">
+        <div class="cellimg"><img src="https://www.zupimages.net/up/19/09/bv2e.jpg" /></div>
+        <div class="cellid" onClick={this.inter} id="Preno">Prénom Nom<div><a href="#"><i class="fas fa-info" ></i></a></div>
+      
+
+        </div>
+      
+      </div>
+      
+      
+
+    </div>
+    {this.state.  }
+    <div class="cellback">
+      <div class="cellhead"><i class="fas fa-signal"></i><i class="fas fa-wifi"></i>
+        <div style={{float:"right"}}>86%<i class="fas fa-battery-three-quarters"></i></div>
+      </div>
+      <div class="cellhour">15:08<span>mardi 26 mars</span></div>
+    </div>
+  </div>
+</div>
+)
