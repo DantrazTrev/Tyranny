54ded137cde49f3804400e14c7f5f82cccbbd535
Ayush PS <ayushpratap16@gmail.com>
2021-05-23 15:07:39 +0530

    Base : Server down

--- src/components/star/star.js
+++ src/components/star/star.js
@@ -12,12 +12,8 @@ export default class Star extends React.Component {
 }
 
 
-componentWillMount(){
-  
 
-}
-
-  componentDidMount(){
+  componentWillMount(){
     axios.get(`https://the-butterfly.dantraztrev.repl.co/api/game/`)
       .then(res => {
         this.setState(
@@ -33,7 +29,7 @@ componentWillMount(){
       <div class="Star">
        <header>THE <br/> BUTTERFLY</header>
        v. {this.state.version}
-       <button class="sr" onClick={this.props.sutrat}>Start</button>
+       <button class="sr" onClick={this.props.sutrat} disabled={this.state.version===""}>Start</button>
       </div>
     )
   }
